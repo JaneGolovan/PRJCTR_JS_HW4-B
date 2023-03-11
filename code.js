9d83256458019e2032a36e810f847e622a208c5a
JaneGolovan <holovan.jane@gmail.com>
2023-03-11 11:30:56 +0200

    fourth commit

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // отримаємо елементи для роботи
 let themeButton = document.querySelector("#themeButton");
 let lastTurn = document.querySelector("#lastTurn");
-
+const storageKeyDate = "storageKeyDate";
 const storageKey = "themeState";
 const darkTheme = "dark-theme";
 const lightTheme = "light-theme";
@@ -11,19 +11,30 @@ const loadThemeState = () => {
   const themeState = localStorage.getItem(storageKey);
   if (themeState === darkTheme) {
     turnOnDarkTheme();
+  } else {
+    turnOnLightTheme();
   }
+  showCurrentDate();
 };
 // Робимо функцію для збереження теми в локалсторедж
 const saveThemeState = (themeState) => {
   localStorage.setItem(storageKey, themeState);
 };
+// Робимо функцію для збереження дати в локалсторедж
+
+const saveDateState = () => {
+  localStorage.setItem(storageKeyDate, Date.now());
+};
+
 // Робимо функцію для виводу актуальної дати
 const showCurrentDate = () => {
-  const currentDate = new Date().toLocaleString();
+  const dateState = new Date(
+    parseInt(localStorage.getItem(storageKeyDate))
+  ).toLocaleString();
   if (document.body.classList.contains(lightTheme)) {
-    lastTurn.textContent = `Last turn on: ${currentDate}`;
+    lastTurn.textContent = `Last turn on: ${dateState}`;
   } else {
-    lastTurn.textContent = `Last turn off: ${currentDate}`;
+    lastTurn.textContent = `Last turn off: ${dateState}`;
   }
 };
 
@@ -33,7 +44,7 @@ const turnOnLightTheme = () => {
   document.body.classList.remove(darkTheme);
   themeButton.textContent = "Turn off";
   saveThemeState(lightTheme);
-  showCurrentDate();
+  saveDateState();
 };
 
 // Умови зміни теми на темну
@@ -42,7 +53,7 @@ const turnOnDarkTheme = () => {
   document.body.classList.remove(lightTheme);
   themeButton.textContent = "Turn on";
   saveThemeState(darkTheme);
-  showCurrentDate();
+  saveDateState();
 };
 
 // міняємо тему залежно від класу
@@ -52,6 +63,7 @@ const changeTheme = () => {
   } else {
     turnOnLightTheme();
   }
+  showCurrentDate();
 };
 
 themeButton.addEventListener("click", changeTheme); //Додаємо слухач події
